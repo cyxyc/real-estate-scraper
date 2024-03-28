817a4870ec25331fde5abdd24aa2e0722e32cc42
Marek Baran ntb <baran.m1505@gmail.com>
2024-03-28 08:45:20 +0100

    google and imgur uploaders

--- imgur_uploader.py
+++ imgur_uploader.py
@@ -1,18 +1,14 @@
-
+from os import path
 from imgurpython import ImgurClient
-import csv
-import ast
 
-client_id = 'your-client-id'
-client_secret = 'your-client-secret'
+client_id = '7c437cedc06d28a'
+client_secret = '170eaa169865ce4f60cb70f0f8aa31b70788b69e'
 
 client = ImgurClient(client_id, client_secret)
 
-with open('df2.csv', 'r') as f:
-    reader = csv.reader(f)
-    next(reader)  # Skip the header
-    for row in reader:
-        image_urls = ast.literal_eval(row[5])  # Parse the string of image URLs into a list
-        for url in image_urls:
-            response = client.upload_from_url(url, config=None, anon=True)
-            print('Imgur URL: ', response['link'])
+path = 'hjk.png'
+image = client.upload_from_path(path)
+
+print("Image was posted!")
+print("You can find it here: {0}".format(image['link']))
+
