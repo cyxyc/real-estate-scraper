b9219ca9bb21897105e5f1deaec442e1a093e1c3
aba1bj <aba1bj@bosch.com>
2024-03-28 11:33:52 +0100

    up

--- imgur_uploader.py
+++ imgur_uploader.py
@@ -1,5 +1,8 @@
 from os import path
 from imgurpython import ImgurClient
+import pandas as pd
+
+df = pd.read_csv('df2.csv')
 
 client_id = '7c437cedc06d28a'
 client_secret = '170eaa169865ce4f60cb70f0f8aa31b70788b69e'
